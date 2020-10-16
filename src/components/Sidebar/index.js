18702db3c7c8b79099e5ed37694e8c6a5135b87a
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-10-16 18:41:39 +0200

    add Sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+} from './SidebarElements';
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="/" onClick={toggle}>Statystyki</SidebarLink>
+          <SidebarLink to="/info" onClick={toggle}>Informacje o COVID-19</SidebarLink>
+          <SidebarLink to="/about" onClick={toggle}>O Projekcie</SidebarLink>
+
+        </SidebarMenu>
+      </SidebarWrapper>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar;
