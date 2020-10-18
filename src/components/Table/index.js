b65653f41728ec44c5e8a4a6496c6d2fd314358b
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-10-18 20:52:41 +0200

    delete translateCountryName function

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,21 +1,9 @@
 import React from 'react'
 import { TableWrapper, TableRow, TableCell } from './TableElements';
 import numeral from "numeral";
-import iso_countries from "i18n-iso-countries";
-
-const translateCountryName = (country, language) => {
-  let countryName;
-  if (country.countryInfo.iso3 === null) {
-    countryName = country.country;
-  }
-  else {
-    countryName = iso_countries.getName(country.countryInfo.iso3, language);
-  }
-  return countryName;
-};
+import { translateCountryName } from '../../util';
 
 const Table = ({ countries, isExpand }) => {
-  iso_countries.registerLocale(require("i18n-iso-countries/langs/pl.json"));
 
   return (
     <TableWrapper isExpand={isExpand}>
@@ -23,7 +11,7 @@ const Table = ({ countries, isExpand }) => {
         <TableRow>
           <TableCell>{translateCountryName(country, "pl")}</TableCell>
           <TableCell>
-            <strong>{numeral(country.cases - country.recovered).format("0,0")}</strong>
+            <strong>{numeral(country.cases).format("0,0")}</strong>
           </TableCell>
         </TableRow>
       ))
