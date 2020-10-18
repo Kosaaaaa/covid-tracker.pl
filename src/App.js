c5c3c6ec885e089fe8ec8736c701f8fe106426e4
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-10-18 02:59:11 +0200

    add info page route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import About from './pages/About';
+import Info from './pages/Info';
+
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} exact />
+        <Route path="/info" component={Info} exact />
         <Route>
           <NotFound />
         </Route>
