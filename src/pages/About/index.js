ebce040bed363c1878639ec049da0c5b300d7fd5
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-10-18 03:30:08 +0200

    fix typo

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -20,7 +20,7 @@ const AboutPage = () => {
             <Typography className="about__typo">
               Projekt powstał na potrzeby konkursu Hackheros. Projekt ma na celu informowanie o aktualnym przebiegu pandemii koronawirusa w Polsce i w reszcie świata w postaci statystyk.
               </Typography>
-            <Typography className="about__typo">Strona ma również sekcję informacyjną w której można się dowiedzieć więcej na temat koronawirusa i jak sobie z nim radzić. Mozna się tam równiez dowiedzieć o tym jak dbać o swoje zdrowie psychiczne oraz innych.</Typography>
+            <Typography className="about__typo">Strona ma również sekcję informacyjną w której można się dowiedzieć więcej na temat koronawirusa i jak sobie z nim radzić. Można się tam równiez dowiedzieć o tym jak dbać o swoje zdrowie psychiczne oraz innych.</Typography>
             <Typography>Źródła danych: <a href="https://disease.sh/">disease.sh</a> <a href="https://www.who.int/">who.int</a></Typography>
             <Typography>Wykonanie: <a href="https://github.com/kosaaaaa" className="about__authorLink">Oskar Kosobucki</a></Typography>
           </CardContent>
