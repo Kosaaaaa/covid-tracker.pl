68b8ef8e6ea24f4c1de0245104600c2db04d2113
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-10-18 03:02:07 +0200

    add responsive font-size and fix margin

--- src/components/Emoji/EmojiElement.js
+++ src/components/Emoji/EmojiElement.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 export const EmojiWrap = styled.span`
-  margin: 0 1rem;
+  margin-left:1rem;
   font-size:2.2rem;
   
+  @media screen and (max-width: 768px){
+    font-size:1.7rem;
+  }
+
 `;
