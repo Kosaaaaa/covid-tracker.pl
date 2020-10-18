2baa622ef6975711d912c53d552b41a84e14cc73
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-10-18 03:08:43 +0200

    add data to InfoPage

---
+++ src/pages/Info/data.js
@@ -0,0 +1,65 @@
+export const section1 = {
+  title: 'COVID-19',
+  p1: `Choroba koronawirusowa (COVID-19) to choroba zakaźna wywoływana przez nowo odkrytego koronawirusa.`,
+  p2: `U większości osób, które zachorują na COVID-19, wystąpią objawy o łagodnym lub umiarkowanym nasileniu. Takie osoby wyzdrowieją bez specjalnego leczenia.`,
+}
+
+export const section2 = {
+  title: `Sposób rozprzestrzeniania się choroby`,
+  p1: `Wirus wywołujący COVID-19 jest przenoszony głównie przez kropelki, które wydzielają się, gdy osoba zarażona kaszle, kicha i wydycha powietrze. Kropelki te są zbyt ciężkie, aby unosić się w powietrzu, dlatego szybko opadają na podłogi i powierzchnie.`,
+  p2: `Do zakażenia poprzez wdychanie wirusa może dojść wtedy, gdy w pobliżu znajduje się osoba chorująca na COVID-19 lub poprzez dotknięcie skażonej powierzchni, a następnie oczu, nosa lub ust.`,
+}
+
+export const healthInfoSymptoms = {
+  p1: `Każdy przypadek COVID-19 ma indywidualny przebieg. U większości zarażonych osób rozwiną się objawy o łagodnym lub umiarkowanym nasileniu. Takie osoby wyzdrowieją bez konieczności hospitalizacji.`,
+  subtitle1: `Najczęściej występujące objawy:`,
+  ul1: ['gorączka', 'suchy kaszel', 'zmęczenie'],
+  subtitle2: `Rzadziej występujące objawy:`,
+  ul2: ['ból mięśni', 'ból gardła', 'biegunka', 'zapalenie spojówek', 'ból głowy', 'utrata smaku lub węchu', 'wysypka skórna lub przebarwienia palców u rąk i stóp']
+}
+
+export const healthInfoPrevention = {
+  p1: `Chroń siebie i osoby w Twoim środowisku, poznając fakty i podejmując odpowiednie środki ostrożności. Postępuj zgodnie z zaleceniami lokalnej stacji sanitarno-epidemiologicznej.`,
+  subtitle1: 'Aby zapobiec rozprzestrzenianiu się COVID-19:',
+  ul1: ['Często myj ręce. Używaj wody z mydłem i środka dezynfekującego na bazie alkoholu.', 'Zachowaj bezpieczną odległość od osób kaszlących i kichających.', 'Noś maskę, gdy zachowanie odległości nie jest możliwe.', 'Nie dotykaj oczu, nosa ani ust.', 'Gdy kaszlesz lub kichasz, zakrywaj nos i usta wewnętrzną stroną łokcia lub chusteczką.', 'Jeśli źle się czujesz, zostań w domu.', 'Jeśli masz gorączkę, kaszel i trudności z oddychaniem, skontaktuj się z lekarzem.'],
+  p2: `Zadzwoń z wyprzedzeniem do świadczeniodawcy, aby szybko uzyskać skierowanie do właściwej placówki medycznej. Zapewni Ci to ochronę i zapobiegnie rozprzestrzenianiu się wirusów i innych czynników zakaźnych.`,
+}
+
+export const healthInfoTreatment = {
+  p1: `Obecnie nie istnieją żadne specjalne szczepionki ani leki przeciwko COVID-19. Badane są różne rodzaje terapii, które zostaną przetestowane w drodze badań klinicznych.`,
+  subtitle1: `Samoopieka`,
+  p2: `Jeśli źle się czujesz, odpoczywaj, pij dużo płynów i jedz posiłki bogate w różnorodne składniki odżywcze. Przebywaj w innym pomieszczeniu niż pozostali członkowie rodziny i, jeśli to możliwe, korzystaj z oddzielnej łazienki. Czyść i dezynfekuj często dotykane powierzchnie.`,
+  p3: `Każdy powinien zadbać o zdrowy styl życia podczas pobytu w domu. Utrzymuj zdrową dietę, dbaj o sen i aktywność fizyczną, a z bliskimi kontaktuj się przez telefon lub Internet. W tych trudnych czasach dzieci potrzebują dodatkowej troski i uwagi ze strony dorosłych. W miarę możliwości wykonuj czynności, które zazwyczaj znajdują się w Twoim planie dnia.`,
+  p4: `To normalne, że podczas kryzysu odczuwa się smutek, stres i dezorientację. Pomóc może rozmowa z zaufanymi osobami, takimi jak znajomi czy członkowie rodziny. Jeśli czujesz, że sytuacja Cię przerasta, skontaktuj się z pracownikiem służby zdrowia lub odpowiednim doradcą.`,
+}
+
+export const accordionsData = {
+  panel1a: {
+    title: 'Zastopuj. Odetchnij. Zastanów się.',
+    p1: 'Odetchnij powoli kilka razy: wciągaj powietrze przez nos i wolno je wydychaj.',
+    p2: 'Powolne oddychanie to jeden z najlepszych sposobów na zmniejszenie stresu, ponieważ wówczas mózg otrzymuje polecenie rozluźnienia ciała.',
+    p3: 'Zwróć uwagę na to, jak się czujesz i o czym myślisz, bez oceniania tego. Zamiast reagować na te myśli i odczucia, przyjmij je do wiadomości i pozwól im przeminąć.',
+  },
+  panel2a: {
+    title: 'Kontaktuj się z innymi',
+    p1: 'Pomóc może rozmowa z zaufanymi osobami. Utrzymuj regularny kontakt z bliskimi osobami. Rozmawiaj z nimi o swoich uczuciach i troskach.'
+  },
+  panel3a: {
+    title: 'Prowadź zdrowy styl życia',
+    subtitle1: 'Co robić:',
+    ul1: ['Wstawaj i kładź się spać codziennie o zbliżonej porze.', 'Utrzymuj higienę osobistą.', 'Jedz zdrowe posiłki o regularnych porach.', 'Regularnie wykonuj ćwiczenia fizyczne. Pomóc mogą już 3–4 minuty ruchu o niewielkiej intensywności, jak np. chodzenie czy rozciąganie.', 'Wydziel sobie czas na pracę i czas na odpoczynek.', 'Wygospodaruj sobie czas na przyjemności.', 'Regularnie rób przerwy podczas siedzenia przed ekranem.'],
+    subtitle2: 'Czego nie robić:',
+    ul2: ['W ramach radzenia sobie ze strachem, niepokojem, nudą i izolacją społeczną nie pij alkoholu i nie bierz narkotyków.',],
+  },
+  panel4a: {
+    title: 'Zadbaj o siebie i innych',
+    p1: 'Nie wymagaj od siebie za dużo w trudne dni. Pogódź się z faktem, że Twoja produktywność w niektóre dni może być wyższa niż w inne.',
+    p2: 'Spróbuj ograniczyć oglądanie, czytanie czy słuchanie wiadomości, które wywołują u Ciebie niepokój czy zmartwienie. Szukaj najnowszych informacji z zaufanych źródeł o określonych porach dnia.',
+    p3: 'Pomaganie innym może być także dla Ciebie korzystne. Jeśli możesz, zaoferuj pomoc potrzebującym osobom ze swojej społeczności.',
+
+  },
+  panel5a: {
+    title: 'Poproś o pomoc, jeśli potrzeba',
+    p1: 'Nie wahaj się szukać profesjonalnej pomocy, jeśli sądzisz, że jej potrzebujesz. Najlepiej zacząć od lekarza pierwszego kontaktu. Pomoc można znaleźć także, dzwoniąc na infolinię.',
+  }
+}
